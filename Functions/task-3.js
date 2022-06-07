ad350a549dcf1dd737d94c8dc19d69a378c03e4d
Mariam Vashakidze <vashakidzemariam27@gmail.com>
2022-06-07 18:38:23 +0400

    functions task 3 completed

--- Functions/task-3.js
+++ Functions/task-3.js
@@ -1,18 +1,12 @@
-function sayHello(){
-    console.log('hello');
+function genericFunction(func) {
+  try {
+    func();
+  } catch (Err) {
+    console.log(err.message);
+  }
+}
+
+function sayHello() {
+  console.log("hello");
 }
 genericFunction(sayHello);
-
-// result:
-'hello';
-
-function addOne(){
-    const a = 2;
-    a++;
-    console.log(a)
-}
-genericFunction(addOne);
-
-// result:
-'Assignment to constant variable'
-
