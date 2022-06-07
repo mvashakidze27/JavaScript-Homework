fc7a96d60cf496a69420405117c4d97f64d56ff9
Mariam Vashakidze <vashakidzemariam27@gmail.com>
2022-06-07 14:17:13 +0400

    functions task 1 completed

--- Functions/task-1.js
+++ Functions/task-1.js
@@ -1,8 +1,23 @@
-const arr = [1,2];
-console.log(arr); // [1,2]
-arr.push(5);
-console.log(arr) // [1,2,5];
+function getDivisors(num) {
+  if (typeof num !== "number") {
+    throw new Error("Error: parameter type is not a Number");
+  }
+  if (num <= 0) {
+    throw new Error("Error: parameter can't be a " + num);
+  }
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
 
-getDivisors(12); // [1, 2, 3, 4, 6, 12]
-getDivisors("Content"); // Error: parameter type is not a Number
-getDivisors(0); // Error: parameter can't be a 0
+try {
+  console.log(getDivisors(12)); // [1, 2, 3, 4, 6, 12]
+  getDivisors("Content"); // Error: parameter type is not a Number
+  getDivisors(0); // Error: parameter can't be a 0
+} catch (err) {
+  console.log(err.message);
+}
