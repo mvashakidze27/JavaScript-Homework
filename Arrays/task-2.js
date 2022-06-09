65ceb623cade2df0bc80ec72389ab6a6653efabd
Mariam Vashakidze <vashakidzemariam27@gmail.com>
2022-06-09 20:34:09 +0400

    arrays folder and tasks files created

---
+++ Arrays/task-2.js
@@ -0,0 +1,2 @@
+const arr = [3,2,1];
+reverse(arr); // [1,2,3]
---
+++ Arrays/task-3-bonus.js
@@ -0,0 +1,4 @@
+const arr = [1,2,3];
+rotate(arr, 1, 'left');  // result: [2,3,1]
+rotate(arr, 1);  // result: [3,1,2]
+rotate(arr, 2);  // result: [2,3,1]
