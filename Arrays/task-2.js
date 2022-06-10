8f1dd966654bbd88bfe144cc4fff8e4501fedb15
Mariam Vashakidze <vashakidzemariam27@gmail.com>
2022-06-10 17:54:36 +0400

    array task-2 finished

--- Arrays/task-2.js
+++ Arrays/task-2.js
@@ -1,2 +1,20 @@
-const arr = [3,2,1];
-reverse(arr); // [1,2,3]
+function reverse(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("Error: parameter should be an array");
+  }
+  if (arr.length === 0) {
+    throw new Error("Error: an array can't be empty");
+  }
+  for (let i = 0; i < arr.length / 2; i++) {
+    temp = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
+try {
+  const arr = [3, 2, 1];
+  console.log(reverse(arr));
+} catch (err) {
+  console.log(err.message);
+}
