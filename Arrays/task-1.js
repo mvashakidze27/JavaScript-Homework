93210271e11747db003ce816a928a4475501194b
Mariam Vashakidze <vashakidzemariam27@gmail.com>
2022-06-10 16:08:44 +0400

    array task-1 finished

--- Arrays/task-1.js
+++ Arrays/task-1.js
@@ -1,10 +1,29 @@
-const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
-f(arr); // 12
-const arr2 = [[[[[1,2]]]]];
-f(arr2); // 3
-const arr3 = [[[[[1,2]]],2],1];
-f(arr3); // 6
-const arr4 = [[[[[]]]]];
-f(arr4); // 0
-const arr5 = [[[[[],3]]]];
-f(arr5); // 3
+let arraySum = 0;
+function f(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("parameter should be an array");
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") {
+      arraySum += arr[i];
+    } else if (Array.isArray(arr[i]) === true) {
+      f(arr[i]);
+    } else {
+      throw new Error("parameter should be a number or an array");
+    }
+  }
+  return arraySum;
+}
+
+try {
+  const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
+  // const arr2 = [[[[[1, 2]]]]];
+  // const arr3 = [[[[[1, 2]]], 2], 1];
+  // const arr4 = [[[[[]]]]];
+  //  const arr5 = [[[[[], 3]]]];
+  // const arr6 = ['hello', 'world']
+  console.log(f(arr));
+} catch (err) {
+  console.log(err.message);
+}
