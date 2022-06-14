40327ca360da9f26204f8ee2ff56f59149e12dac
Mariam Vashakidze <vashakidzemariam27@gmail.com>
2022-06-14 18:50:28 +0400

    string task-1 was by mistake so I corrected it

--- Strings/task-1.js
+++ Strings/task-1.js
@@ -1,22 +1,13 @@
-function reverseCase(sentence) {
-    if (typeof sentence !== "string") {
-      throw new Error("Error: parameter type is not a String");
-    }
-    for (let i = 0; i < sentence.length; i++) {
-      if (sentence == sentence.toUpperCase()) {
-        sentence.toUpperCase(sentence.toLowerCase());
-      } else if (sentence == sentence.toLowerCase()) {
-        sentence.toLowerCase(sentence.toUpperCase());
-      }
-    }
-    return sentence;
-  }
-  
-  try {
-    console.log(reverseCase("Hello theRe"));
-    reverseCase("BIG LETTERS");
-    reverseCase("");
-  } catch (err) {
-    console.log(err.message);
-  }
+function rmHtmlTags(str) {
+  if (typeof str !== "string") {
+    throw new Error("Error: parameter type is not a String");
+  } else str = str.toString();
+  return str.replace(/(<([^>]+)>)/g, "");
+}
+
+try {
+  console.log(rmHtmlTags("<p><strong><em>Content</em></strong></p>"));
+} catch (err) {
+  console.log(err.message);
+}
   
