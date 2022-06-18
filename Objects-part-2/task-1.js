73ba778460fdbd984a2694f8ccf35f7eef6d1123
Mariam Vashakidze <vashakidzemariam27@gmail.com>
2022-06-18 18:31:26 +0400

    objects part 2 task 1 completed

---
+++ Objects-part-2/task-1.js
@@ -0,0 +1,45 @@
+Object.defineProperty(Object.prototype, "mergeDeepRight", {
+  value: function (obj) {
+    let arr = Object.get0wnPropertyNames(obj);
+    for (let el of arr) {
+      if (
+        typeof obj[el] === "object" &&
+        !Array.isArray(obj[el]) &&
+        typeof this[el] === "object" &&
+        !Array.isArray(this[el])
+      ) {
+        this[el].mergeDeepRight(obj[el]);
+      } else if (Array.isArray(obj[el]) && Array.isArray(this[el])) {
+        this[el] = this[el].concat(obj[el]);
+      } else {
+        this[el] = obj[el];
+      }
+    }
+  },
+});
+
+const data = {
+  name: "fred",
+  age: 10,
+  contact: {
+    email: "moo@example.com",
+    meta: {
+      verified: true,
+      tags: ["important"],
+    },
+  },
+};
+
+data.mergeDeepRight({
+  age: 40,
+  contact: {
+    email: "baa@example.com",
+    favorite: true,
+    meta: {
+      tags: ["vip"],
+    },
+  },
+});
+
+console.log(data);
+console.log(data.contact.meta.tags);
